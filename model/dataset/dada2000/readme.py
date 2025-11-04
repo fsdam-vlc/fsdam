21ec52af420932a186411357bdbbab50958ae2c0
fsdam-vlc <kaiser.drive1@gmail.com>
2025-11-04 00:56:52 -0600

    Update readme.py

--- model/dataset/dada2000/readme.py
+++ model/dataset/dada2000/readme.py
@@ -1,2 +1,2 @@
-Two folder of DADA-2000 test set: `camera` & `gaze`.
-They provided a json file mentioning `video # & clip id`. We followed official repo instructions.
+Two folder of DADA: test set: `camera` & `gaze`.
+They provided a json file mentioning `video` # & `clip id`. We followed official repo instructions.
